9a041bdfeaa3395a1019ce827c25ec899aae9c84
Kusutan <kusutan2019@gmail.com>
2019-07-15 20:40:46 +0900

    計算式が複雑で見にくいのを修正した

--- opai.py
+++ opai.py
@@ -10,36 +10,17 @@ class New:
     def __calculateOppaiY(self, x):
         if self.type == 'bunkyo':
             e = numpy.e
-            y_1 = (0.8 * (e ** ((-x ** 2) / 2)))
-            y_2 = (e ** (-(x - 1.4) ** 2))
-            y_3 = ((1 / 6) * (e ** (-(2 * x - 4.17) ** 2)))
-            y_4 = ((1 / 8) * (e ** (-(6.9 * x - 8.3) ** 2)))
-            y = y_1 + y_2 + y_3 + y_4
-
-            return y
+            return (0.8 * (e ** ((-x ** 2) / 2))) + (e ** (-(x - 1.4) ** 2)) + \
+                   ((1 / 6) * (e ** (-(2 * x - 4.17) ** 2))) + ((1 / 8) * (e ** (-(6.9 * x - 8.3) ** 2)))
 
         elif self.type == 'nagoya':
-            y_1 = (1.2 * numpy.exp(-0.5 * x ** 2))
-            y_2 = (numpy.exp(0.1 * x) - 1.3 ** (0.6 * x) - 1.1 ** -x)
-            y_3 = (0.2 * numpy.exp(-7 * (x - 0.8) ** 8))
-            y_4 = (0.1 * numpy.exp(-(8 * x - 2) ** 2) - 1.1 ** (x - 6))
-            y_5 = (0.4 * numpy.exp(-0.04 * x ** 8))
-            y_6 = (0.4 * numpy.exp(-0.04 * (x + 3) ** 8))
-            y_7 = (1.2)
-            y = y_1 + y_2 + y_3 + y_4 + y_5 + y_6 + y_7
-
-            return y
+            return (1.2 * numpy.exp(-0.5 * x ** 2)) + (numpy.exp(0.1 * x) - 1.3 ** (0.6 * x) - 1.1 ** -x) + \
+                   (0.2 * numpy.exp(-7 * (x - 0.8) ** 8)) + (0.1 * numpy.exp(-(8 * x - 2) ** 2) - 1.1 ** (x - 6)) + \
+                   (0.4 * numpy.exp(-0.04 * x ** 8)) + (0.4 * numpy.exp(-0.04 * (x + 3) ** 8)) + (1.2)
 
         else:
-            y_1 = ((numpy.sqrt((numpy.absolute(1 - x) + 1 - x) / 2) + (1 / 4)) * numpy.exp(-(1 - x) ** 2))
-            y_2_1 = (1 / 40)
-            y_2_2 = (numpy.absolute(1 - 2 ** 4 * (5 * x - 3) ** 4))
-            y_2_3 = (numpy.absolute(1 - 2 ** 9 * (5 * x - 3) ** 4))
-            y_2_4 = (2 - 528 * (5 * x - 3) ** 4)
-            y_2 = (y_2_1 * (y_2_2 + y_2_3 + y_2_4))
-            y = y_1 + y_2
-
-            return y
+            return ((numpy.sqrt((numpy.absolute(1 - x) + 1 - x) / 2) + (1 / 4)) * numpy.exp(-(1 - x) ** 2)) + \
+                   (1 / 40) * ((numpy.absolute(1 - 2 ** 4 * (5 * x - 3) ** 4)) + (numpy.absolute(1 - 2 ** 9 * (5 * x - 3) ** 4)) + (2 - 528 * (5 * x - 3) ** 4))
 
     # おっぱい描画のロジックは見せない
     def __plot(self, x, y):
